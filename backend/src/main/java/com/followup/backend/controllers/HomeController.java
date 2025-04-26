2f6b363ae0c4c44856b25ee3adadc8516b0dea55
nyazul <ansarinyazul2003.com>
2025-04-26 11:42:56 +0530

    first commit

---
+++ backend/src/main/java/com/followup/backend/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.followup.backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping("/")
+    public String landingPage() {
+        return "index";
+    }
+
+}
